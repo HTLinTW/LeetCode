67e76a05dd50ab652f2e97411128ca9bfb491334
林宏村(HongtsuenLin) <hongtsuenlin@chipbond.com.tw>
2021-12-02 17:13:59 +0800

    T3_OK

---
+++ Project1/Project3/F1.cpp
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//主要差在unique的搜尋方式比KEY VALUE快
+//因為不是0開始搜尋
+class Solution {
+public:
+	int unique(string &s, int front, int rear, char c) {
+		for (int i = front; i < rear; ++i) {
+			if (s[i] == c)
+				return i;
+		}
+		return -1;
+	}
+
+	int lengthOfLongestSubstring(string s) {
+		if (s.size() <= 1)
+			return s.size();
+
+		int res = 0;
+		for (
+			int front = 0, rear = 1;
+			rear < s.size();
+			++rear)
+		{
+			int t = unique(s, front, rear, s[rear]);
+			if (t != -1)
+				front = t + 1;
+
+			res = max(res, rear - front + 1);
+		}
+		return res;
+
+	}
+};
+
+
+//我的想法是找到重複時再去算長度，最後結束時再算一次
+//其他方式是讀到每個字元都算一次
+//不論工具的話想法上我的會比較快
+class SolutionPlus {
+public:
+	int unique(string &s, int front, int rear, char c) {
+		for (int i = front; i < rear; ++i) {
+			if (s[i] == c)
+				return i;
+		}
+		return -1;
+	}
+
+	int lengthOfLongestSubstring(string s) {
+		int front = 0, rear = 1;
+		if (s.size() <= 1)
+			return s.size();
+
+		int res = 0;
+		for (
+			front = 0, rear = 1;
+			rear < s.size();
+			++rear)
+		{
+			int t = unique(s, front, rear, s[rear]);
+			if (t != -1)
+			{
+				res = max(res, rear - front);
+				front = t + 1;
+			}
+
+		}
+		res = max(res, rear - 1 - front + 1);
+		return res;
+
+	}
+};
+
+//邏輯改成一樣，但用MAP速度跟VECTOR還是有差
+//比起KEY VALUE，直接位置讀取更快
+//用空間換速度，有預期CHAR的上線值才能用
+class Solution2 {
+public:
+	int lengthOfLongestSubstring(string s) {
+		vector<int>freq(256, -1);
+		int l = 0, r = 0, ans = 0;
+		while (r < s.length()) {
+			if (freq[s[r]] != -1) {
+				l = max(l, freq[s[r]] + 1);
+			}
+			freq[s[r]] = r;
+			ans = max(ans, r - l + 1);
+			r++;
+		}
+		return ans;
+
+	}
+};
+
+int main()
+{
+	Solution2 s;
+
+	string str = "ab";
+	cout << endl;
+	cout << endl << s.lengthOfLongestSubstring(str) << endl;
+	cout << endl;
+}
--- Project1/Project3/T3.cpp
+++ Project1/Project3/T3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -29,53 +30,104 @@ using namespace std;
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
-		int start = -1, max = 0;
+		int start = -1, Max = 0;
 		map<int, int> map;
 		for (int i = 0; i < s.size(); i++)
 		{
 			if (map.count(s[i]) && start < map[s[i]])
 			{
-				if (max < i - 1 - start)
-					max = i - 1 - start;
+				if (Max < i - 1 - start)
+					Max = i - 1 - start;
 				start = map[s[i]];
 			}
 			map[s[i]] = i;
 		}
-		if (max < s.size() - 1 - start && s.size() != 0)
-			max = s.size() - 1 - start;
-		return max;
+		if (Max < s.size() - 1 - start && s.size() != 0)
+			Max = s.size() - 1 - start;
+		return Max;
 	}
 };
 
 class Solution2 {
 public:
 	int lengthOfLongestSubstring(string s) {
-		int start = 1, max = 0;
+		int start = -1, Max = 0;
 		map<int, int> map;
 		for (int i = 0; i < s.size(); i++)
 		{
-			if (start < map[s[i]])
+			if (map.count(s[i]) && start < map[s[i]])
 			{
-				if (max < i +2 - start)
-					max = i +2 - start;
+				Max = max(Max, i - 1 - start);
 				start = map[s[i]];
 			}
-			map[s[i]] = i+1;
+			map[s[i]] = i;
 		}
-		if (max < s.size() +1 - start && s.size() != 0)
-			max = s.size() +1 - start;
-		else if (s.size() == 1)
-			max = 1;
-		return max;
+		if (s.size() != 0)
+			Max = max(Max, (int)(s.size() - 1 - start));
+		return Max;
 	}
 };
-//1234
-int main()
-{
-	Solution2 s;
 
-	string str = "ab";
-	cout << endl;
-	cout << endl << s.lengthOfLongestSubstring(str) << endl;
-	cout << endl;
-}
+//不使用Key Value的話
+//速度還比較快....
+//主要差再搜尋速度
+//雖然map是O(logN)
+//但搜尋重複值的地方像是用O(N)比較久
+//但搜尋重複值是從start開始找，而不是重頭，所以速度快上很多，N比預期的範圍小很多
+//如果是從0開始的話，完整的O(N)時間就會有差了
+class Solution2plus {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int start = -1, Max = 0;
+		for (int i = 0; i < s.size(); i++)
+		{
+			for (int j = start; j < i; j++)
+			{
+				if (j == -1)
+					continue;
+				if (s[i] == s[j] && start < j)
+				{
+					Max = max(Max, i - 1 - start);
+					start = j ;
+					break;
+				}
+			}
+		}
+		if (s.size() != 0)
+			Max = max(Max, (int)(s.size() - 1 - start));
+		return Max;
+	}
+};
+
+//邏輯改成F1的S2，不用VECTOR用map的話速度上還是有差
+class Solution3 {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int start = -1, Max = 0;
+		map<int, int> map;
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (map.count(s[i]))
+			{
+				start = max(start, map[s[i]]);
+			}
+			Max = max(Max, i - start);
+			map[s[i]] = i;
+		}
+		return Max;
+	}
+};
+
+
+
+//1234
+
+//int main()
+//{
+//	Solution2 s;
+//
+//	string str = "ab";
+//	cout << endl;
+//	cout << endl << s.lengthOfLongestSubstring(str) << endl;
+//	cout << endl;
+//}
