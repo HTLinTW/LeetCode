1dc4555fd07f38731ae32fa29a11ec19326ea156
林宏村(HongtsuenLin) <hongtsuenlin@chipbond.com.tw>
2021-12-21 13:16:48 +0800

    T12_OK

--- Project1/Project12/T12.cpp
+++ Project1/Project12/T12.cpp
@@ -8,51 +8,47 @@ using namespace std;
 
 class Solution {
 public:
-	string mixF(char s1, char s2, int i)
+	//5種呈現，再分s2是不是空值的情況
+	string roman(char c1, char c2, int i)
 	{
 		string ans = "";
 		switch (i)
 		{
 		case 0:
-			ans = ans + s1 + s2;
+			ans = ans + c1 + c2;
 			break;
 		case 1:
-			ans = ans + s2;
+			ans = ans + c2;
 			break;
 		case 2:
-			ans = ans + s2 + s1;
+			ans = ans + c2 + c1;
 			break;
 		case 3:
-			ans = ans + s2 + s1 + s1;
+			ans = ans + c2 + c1 + c1;
 			break;
 		case 4:
-			ans = ans + s2 + s1 + s1 + s1;
+			ans = ans + c2 + c1 + c1 + c1;
 			break;
 		default:
 			break;
 		}
+
 		return ans;
 	}
 
-	string mixF(char s1, int i)
+	string roman(char c1, int i)
 	{
 		string ans = "";
 		switch (i)
 		{
-		case 0:
-			ans = ans + s1;
-			break;
-		case 1:
-			ans = ans;
-			break;
 		case 2:
-			ans = ans + s1;
+			ans = ans + c1;
 			break;
 		case 3:
-			ans = ans + s1 + s1;
+			ans = ans + c1 + c1;
 			break;
 		case 4:
-			ans = ans + s1 + s1 + s1;
+			ans = ans + c1 + c1 + c1;
 			break;
 		default:
 			break;
@@ -61,17 +57,21 @@ public:
 	}
 
 	string intToRoman(int num) {
-		string s = " IVXLCDM";
+		string s = "IVXLCDM";
 		string ans = "";
 		int i = 0, j = 0, k = 0;
+		//	i for I,X,C,M
+		//	j for telling 0~3,4~8,9
+		//	k for 5 mode
 		while (num > 0)
 		{
 			k = (((num + 1) % 10) % 5);
 			j = (((num % 10) + 1) / 5);
+			//只有j在0時才會有空值(0~3)
 			if (j == 0)
-				ans = mixF(s[i + 1], k) + ans;
+				ans = roman(s[i], k) + ans;
 			else
-				ans = mixF(s[i + 1], s[i + j + 1], k) + ans;
+				ans = roman(s[i], s[i + j], k) + ans;
 			num /= 10;
 			i += 2;
 		}
